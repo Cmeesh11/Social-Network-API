a83275872cb1f01e5c1fed5b905b242ac92bdfa1
Cmeesh11 <cartermeesh11@gmail.com>
2023-05-08 12:39:34 -0400

    created get and post routes for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,15 @@ module.exports = {
     } catch (err) {
       res.status(500).json(err);
     }
-  }
+  },
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async
 
 }
